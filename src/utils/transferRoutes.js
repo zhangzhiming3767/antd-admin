44aa1d2c258d99a26dcda0f26163090272c37e67
wb-zzm508744 <wb-zzm508744@alibaba-inc.com>
2019-11-05 17:39:30 +0800

    Initial commit

---
+++ src/utils/transferRoutes.js
@@ -0,0 +1,13 @@
+function transferRoutes(list){
+    if(list && list.length){
+        let routes=[]
+        routes.push({
+            id:'-1',
+            icon:'dashboard',
+            route:'/dashboard',
+            zh:{
+                name:'工作台',
+            }
+        })
+    }
+}
